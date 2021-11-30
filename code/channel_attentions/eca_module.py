12701bd06109f5e385a78cc3657d6ee7db83d07f
MenghaoGuo <38318671+MenghaoGuo@users.noreply.github.com>
2021-11-30 18:38:15 +0800

    Create eca_module.py

---
+++ code/channel_attentions/eca_module.py
@@ -0,0 +1,37 @@
+import jittor as jt 
+import jittor.nn as nn
+
+class ECALayer(nn.Module):
+    """
+    Constructs a ECA module.
+    Args:
+        k_size: Adaptive selection of kernel size
+    """
+    def __init__(self, k_size=3):
+        super(ECALayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False) 
+        self.sigmoid = nn.Sigmoid()
+
+    def execute(self, x):
+        # feature descriptor on the global spatial information
+        y = self.avg_pool(x)
+
+        # Two different branches of ECA module
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+
+        y = self.sigmoid(y)
+
+        return x * y.expand_as(x)
+
+
+def main():
+    attention_blcok = ECALayer()
+    input = jt.rand([4, 64, 32, 32])
+    output = attention_blcok(input)
+    print (input.size(), output.size())
+
+if __name__ == '__main__':
+    main()
+
+
