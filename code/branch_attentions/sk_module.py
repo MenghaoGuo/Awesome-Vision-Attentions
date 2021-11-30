32cf32cca972a2c308941c15068351ef266b577e
MenghaoGuo <38318671+MenghaoGuo@users.noreply.github.com>
2021-11-30 21:15:34 +0800

    Update sk_module.py

--- code/branch_attentions/sk_module.py
+++ code/branch_attentions/sk_module.py
@@ -1,1 +1,69 @@
 
+import jittor as jt 
+import jittor.nn as nn
+
+class SKModule(nn.Module):
+    def __init__(self, features, M=2, G=32, r=16, stride=1 ,L=32):
+        """ Constructor
+        Args:
+            features: input channel dimensionality.
+            M: the number of branchs.
+            G: num of convolution groups.
+            r: the ratio for compute d, the length of z.
+            stride: stride, default 1.
+            L: the minimum dim of the vector z in paper, default 32.
+        """
+        super(SKModule, self).__init__()
+        d = max(int(features/r), L)
+        self.M = M
+        self.features = features
+        self.convs = nn.ModuleList([])
+        for i in range(M):
+            self.convs.append(nn.Sequential(
+                nn.Conv2d(features, features, kernel_size=3, stride=stride, padding=1+i, dilation=1+i, groups=G, bias=False),
+                nn.BatchNorm2d(features),
+                nn.ReLU()
+            ))
+        self.gap = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Sequential(nn.Conv2d(features, d, kernel_size=1, stride=1, bias=False),
+                                nn.BatchNorm2d(d),
+                                nn.ReLU())
+        self.fcs = nn.ModuleList([])
+        for i in range(M):
+            self.fcs.append(
+                 nn.Conv2d(d, features, kernel_size=1, stride=1)
+            )
+        self.softmax = nn.Softmax(dim=1)
+        
+    def execute(self, x):
+        
+        batch_size = x.shape[0]
+        
+        feats = [conv(x) for conv in self.convs]      
+        feats = jt.concat(feats, dim=1)
+        feats = feats.view(batch_size, self.M, self.features, feats.shape[2], feats.shape[3])
+        
+        feats_U = jt.sum(feats, dim=1)
+        feats_S = self.gap(feats_U)
+        feats_Z = self.fc(feats_S)
+
+        attention_vectors = [fc(feats_Z) for fc in self.fcs]
+        attention_vectors = jt.concat(attention_vectors, dim=1)
+        attention_vectors = attention_vectors.view(batch_size, self.M, self.features, 1, 1)
+        attention_vectors = self.softmax(attention_vectors)
+        
+        feats_V = jt.sum(feats*attention_vectors, dim=1)
+        
+        return feats_V
+
+
+def main():
+    attention_blcok = SKModule(64)
+    input = jt.rand([4, 64, 32, 32])
+    output = attention_blcok(input)
+    print (input.size(), output.size())
+
+if __name__ == '__main__':
+    main()
+
+
